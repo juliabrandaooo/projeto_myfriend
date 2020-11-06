de5518aab743930549efaa80492cff3613c3e996
Klecia Vladmaker <klecia.vladmaker@hotmail.com>
2020-11-06 09:16:26 -0300

    Validar fale conosco

--- scripts/faleconosco.js
+++ scripts/faleconosco.js
@@ -1,3 +1,36 @@
-function Teste (){
-    
+function validarFormFaleConosco() {
+    var nome = formfaleconosco.nome.value;
+    var email = formfaleconosco.email.value;
+    var assunto = formfaleconosco.assunto.value;
+    var msg = formfaleconosco.msg.value;
+
+
+    if (nome == "") {
+        alert("Campo nome é obrigatório!");
+        formfaleconosco.nome.focus();
+        return false;
+    }
+    if (email == "") {
+        alert("Campo email é obrigatório!");
+        formfaleconosco.email.focus();
+        return false;
+    }
+    if (assunto == "") {
+        alert("Campo assunto é obrigatório!");
+        formfaleconosco.assunto.focus();
+        return false;
+    }
+    if (msg == "") {
+        alert("Campo mensagem é obrigatório!");
+        formfaleconosco.msg.focus();
+        return false;
+    }
+
+    function validaEmail(email) {
+
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+
+    }
+
 }
